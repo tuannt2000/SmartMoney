f4ab27f69545ec5059d6fd34e5f9684403eb1aec
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2021-10-30 16:41:08 +0700

    commit

--- resources/views/wallet/settings/general.blade.php
+++ resources/views/wallet/settings/general.blade.php
@@ -140,6 +140,43 @@
         </div>
     </div>
 
+
+    <!-- Xóa ví -->
+    <div class="ReactModalPortal">
+        <div class="ReactModal__Overlay">
+            <div class="ReactModal__Content ReactModal__Content--after-open _2RxW" style="z-index: 1000;">
+                <div class="_3EUw">
+                    <form action="">
+                        <article class="_2ZwS ">
+                            <div class="_13pY">
+                                <div class="_36Dk">
+                                    <button type="button" class="egUi B8bm ">
+                                        <span class="_2nf9">
+                                            <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="import-step-1" transform="translate(-1252.000000, -166.000000)" fill="currentColor"><g id="import-dialog" transform="translate(420.000000, 158.000000)"><g transform="translate(824.000000, 0.000000)"><path d="M13,13 L8.00684547,13 C7.44994876,13 7,13.4477153 7,14 C7,14.5561352 7.45078007,15 8.00684547,15 L13,15 L13,19.9931545 C13,20.5500512 13.4477153,21 14,21 C14.5561352,21 15,20.5492199 15,19.9931545 L15,15 L19.9931545,15 C20.5500512,15 21,14.5522847 21,14 C21,13.4438648 20.5492199,13 19.9931545,13 L15,13 L15,8.00684547 C15,7.44994876 14.5522847,7 14,7 C13.4438648,7 13,7.45078007 13,8.00684547 L13,13 Z" id="Combined-Shape" transform="translate(14.000000, 14.000000) rotate(45.000000) translate(-14.000000, -14.000000) "></path></g></g></g></g></svg>
+                                        </span>
+                                    </button>
+                                </div>
+                                <div class="_2GnZ">
+                                    <div class="_1bps">
+                                        <svg width="24" height="32" viewBox="0 0 12 16" xmlns="http://www.w3.org/2000/svg"><g><path d="M1 14.222C1 15.2 1.75 16 2.667 16h6.666C10.25 16 11 15.2 11 14.222V3.556H1v10.666zM12 1H9l-.857-1H3.857L3 1H0v1.667h12V1z"></path></g></svg>
+                                    </div>
+                                </div>
+                                <h1 class="">Delete wallet Ví tiền mặt</h1>
+                                <div class="j4fJ">
+                                    <p>Are you sure you want to delete the wallet Ví tiền mặt? You will lose all your transactions, budgets and overview inside of this wallet.</p>
+                                </div>
+                            </div>
+                            <div class="_2axu">
+                                <button type="submit" class="_3SdL _1_Kr ">Xóa ví</button>
+                                <a class="_3WuR _1gOp ">Hủy</a>
+                            </div>
+                        </article>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @section("script")
